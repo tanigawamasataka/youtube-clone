3d89c4e0ad443426e03aab01497c7b2c994be700
tanigawa.masataka@gmail.com <tanigawa.masataka@gmail.com>
2020-12-31 16:11:24 +0900

    Layout+Headerコンポーネント作成

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Top = () => {
     return (
-        <div>
-           top page
-        </div>
+        <Layout>
+            Top page
+        </Layout>
     )
 }
 
